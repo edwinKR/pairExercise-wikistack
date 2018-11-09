18dafb0d6343b09ccb496e37c18190693041de62
Jay Jongik Thom <ljik95@hotmail.com>
2018-11-09 10:50:53 -0600

    installed sequelize

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const layout = require('./views/layout')
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get()
+app.get('/', (req, res) => {
+  res.send(layout());
+});
+
+const PORT = '3000';
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+});
