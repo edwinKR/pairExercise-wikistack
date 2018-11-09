6f661584a8487a07b50e59d4bb71767009935247
Jay Jongik Thom <ljik95@hotmail.com>
2018-11-09 11:29:54 -0600

    Finished Sequelize part

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 const layout = require('./views/layout')
+const {db} = require('./models');
+const models = require('./models');
 const app = express();
+
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,7 +16,19 @@ app.get('/', (req, res) => {
   res.send(layout());
 });
 
+db.authenticate().then(() => {
+  console.log('connected to the database!!!');
+})
+
 const PORT = '3000';
-app.listen(PORT, () => {
-  console.log(`Listening to ${PORT}`);
-});
+
+const init = async () => {
+  await models.db.sync({force: true})
+
+
+  app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}`);
+  });
+}
+
+init();
